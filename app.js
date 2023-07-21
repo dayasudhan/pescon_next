eec2fdb142ef2456e7173a516d3ff9e86437730b
dayasudhan <dayasudhankg@gmail.com>
2023-07-21 16:56:48 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const next = require('next')
-
+require('dotenv').config();
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -8,13 +8,13 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
   server.get('/hello', (req, res) => res.send('Namaste Home Page'));
-  server.get('/test', (req, res) => res.send('Namaste Test Page'));
+  server.get('/test', (req, res) => res.send('Namaste Test123 Page'));
   server.get('*', (req, res) => {
     return handle(req, res)
   })
   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`Server is listening on port ${process.env.port}.${process.env.DEV} `)
   })
 })
 .catch((ex) => {
