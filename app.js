4591ec85f35c0074dce2f6cd3b08b29bfa15275a
dayasudhan <dayasudhankg@gmail.com>
2023-07-21 18:04:28 +0530

    rename some variable

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express')
 const next = require('next')
 require('dotenv').config();
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const server = next({ dev })
+const handle = server.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = express()
-  server.get('/hello', (req, res) => res.send('Namaste Home Page'));
-  server.get('/test', (req, res) => res.send('Namaste Test123 Page'));
-  server.get('*', (req, res) => {
+server.prepare().then(() => {
+  const app = express()
+  app.get('/hello', (req, res) => res.send('Namaste Home Page'));
+  app.get('/leads', (req, res) => res.send({'name':'dayasudhan'}));
+  app.get('*', (req, res) => {
     return handle(req, res)
   })
-  server.listen(3000, (err) => {
+  app.listen(3000, (err) => {
     if (err) throw err
     console.log(`Server is listening on port ${process.env.port}.${process.env.DEV} `)
   })
