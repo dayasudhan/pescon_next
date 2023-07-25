16e2ce5b31cfef7bca30a23038a8c5c05497f7a5
dayasudhan <dayasudhankg@gmail.com>
2023-07-25 20:57:08 +0530

    center the page

--- pages/GoogleSignInButton.js
+++ pages/GoogleSignInButton.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import firebase from './firebaseConfig';
-
+import 'semantic-ui-css/semantic.css';
+import { Button } from 'semantic-ui-react'
 const GoogleSignInButton = () => {
   const handleSignIn = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     try {
       await firebase.auth().signInWithPopup(provider);
+      
     } catch (error) {
       console.error('Error signing in with Google:', error);
     }
   };
 
   return (
-    <button onClick={handleSignIn}>
+    <Button primary onClick={handleSignIn}>
       Sign in with Google
-    </button>
+    </Button>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import { useContext } from 'react';
 import { AuthContext } from './authContext';
 import GoogleSignInButton from './GoogleSignInButton';
-//import { Image } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import Image from 'next/image';
-import Home from './home.tsx';
+import { useRouter } from 'next/router';
+import 'semantic-ui-css/semantic.css';
 const Index = () => {
   const { user } = useContext(AuthContext);
+  const router = useRouter();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log("i am inside")
+    router.push('/home');
 
+  };
   return (
-    <div>
-      <h1>Pescon</h1>
+    <div className="login-container">
+    
       <Image
         src="/images/logo1.png"
         alt="Your Image"
@@ -19,23 +26,34 @@ const Index = () => {
       {user ? (
         <div>
           <h1>Welcome, {user.displayName}!</h1>
+          <div className="center-button">
+          <Button primary type="submit"  onClick={handleLogin}>Home</Button>
+        </div>
+          
          
-         
-      {/* <Segment> */}
-        
-        <Home />
-        {/* <Footer /> */}
-      {/* </Segment> */}
-
-
         </div>
       ) : (
         <div>
-
-      <p>Please sign in using Google:</p>
-      <GoogleSignInButton />
-    </div>
+            <p>Please sign in using Google:</p>
+            <GoogleSignInButton />
+        </div>
       )}
+      {/* Your login styles */}
+      <style jsx>{`
+        .login-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 50vh;
+        }
+
+        .center-button {
+          display: flex;
+          justify-content: center;
+          margin-top: 16px;
+        }
+      `}</style>
     </div>
   );
 }
